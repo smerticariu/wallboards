280e01a6c9bb8b161991b1a067420ed2558f0aa1
Sergiu Merticariu <semerticariu@gmail.com>
2021-10-27 17:26:21 +0300

    small fix

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ import axios from 'axios';
 function App() {
   const dispatch = useDispatch();
   const { userInfo, userTokenInfo } = useSelector((state) => state.login);
-  const { isAuthenticated, getAccessTokenSilently, logout, isLoading } = useAuth0();
+  const { isAuthenticated, getAccessTokenSilently, logout } = useAuth0();
   const activeModalName = useSelector((state) => state.modal.activeModalName);
   const { warningMessage } = useSelector((state) => state.modal);
   const sfToken = window?.WbConfig?.sfSessionId;
