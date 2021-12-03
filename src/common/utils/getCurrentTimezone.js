48cd2d402420f7e31f7ef333e00f39c391fb0287
Sergiu Merticariu <semerticariu@gmail.com>
2021-12-03 09:28:49 +0200

    remove unnnecessary code

--- src/common/utils/getCurrentTimezone.js
+++
@@ -1,6 +0,0 @@
-import { DEFAULTS } from '../defaults/defaults';
-
-export const getCurrentTimezone = () => {
-  const currentTimezone = 0 - new Date().getTimezoneOffset();
-  return DEFAULTS.MODAL.CALL_TRACKING.TIME_ZONE.find((timezone) => timezone.id === currentTimezone);
-};
