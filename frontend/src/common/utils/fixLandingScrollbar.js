642738467b445a6d7f2de33a20968b0eeb2adf27
gotca dumitru <dumitru.gotca@assist.ro>
2021-12-17 15:39:34 +0200

    fix unit test

--- frontend/src/common/utils/fixLandingScrollbar.js
+++ frontend/src/common/utils/fixLandingScrollbar.js
@@ -1,6 +1,6 @@
 export const fixLandingScrollBar = () => {
-  const toolbarHeight = document.querySelector('.c-toolbar').offsetHeight;
-  const bannerHeight = document.querySelector('.c-banner').offsetHeight;
+  const toolbarHeight = document.querySelector('.c-toolbar')?.offsetHeight;
+  const bannerHeight = document.querySelector('.c-banner')?.offsetHeight;
   const landingTable = document.querySelector('.c-landing-table');
-  landingTable.style.height = `calc(100vh - ${bannerHeight + toolbarHeight + 30}px)`;
+  if (landingTable?.style?.height) landingTable.style.height = `calc(100vh - ${bannerHeight + toolbarHeight + 30}px)`;
 };
