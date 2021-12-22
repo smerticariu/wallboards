e629f6205a0c7f0f196df59bd463b61c81fa0098
Sergiu Merticariu <semerticariu@gmail.com>
2021-12-22 09:35:45 +0200

    sync

--- src/common/defaults/skills.defaults.js
+++
@@ -1,7 +0,0 @@
-export const SKILLS = {
-  API: {
-    GET: {
-      ALL_SKILLS: 'all skills',
-    },
-  }
-}
--- src/common/utils/percentValue.js
+++
@@ -1,8 +0,0 @@
-export const percentValue = (value, total, precision) => {
-  precision = precision != null ? precision : 1;
-  if (total > 0) {
-    return Number(((value / total) * 100).toFixed(precision));
-  } else {
-    return 0;
-  }
-};
