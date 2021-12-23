1349a0c654352e9300ef76ffef7fc4a652ec9add
Sergiu Merticariu <semerticariu@gmail.com>
2021-12-23 15:46:28 +0200

    update

--- src/common/defaults/toolbar.defaults.js
+++
@@ -1,9 +0,0 @@
-export const TOOLBAR = {
-  NAME: {
-    LANDING: 'landing',
-    ERROR: 'error',
-    NEW_WALLBOARD: 'new-wallboard',
-    NEW_WALLBOARD_GROUP: 'new-wallboard-group',
-    WALLBOARD_READ_ONLY: 'wb-read-only',
-  },
-};
--- src/common/utils/fixLandingScrollbar.js
+++
@@ -1,6 +0,0 @@
-export const fixLandingScrollBar = () => {
-  const toolbarHeight = document.querySelector('.c-toolbar')?.offsetHeight;
-  const bannerHeight = document.querySelector('.c-banner')?.offsetHeight;
-  const landingTable = document.querySelector('.c-landing-table');
-  if (landingTable) landingTable.style.height = `calc(100vh - ${bannerHeight + toolbarHeight + 30}px)`;
-};
